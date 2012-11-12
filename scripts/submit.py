d484e52cae5c91b509726a18aab1efc3738ed4ba
Bryce Boe <bbzbryce@gmail.com>
2012-11-12 14:40:34 -0800

    submit.py script reads settings from nudibranch.ini.

--- scripts/submit.py
+++ scripts/submit.py
@@ -1,28 +1,51 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import base64
 import getpass
 import hashlib
 import json
+import os
 import requests
 import sys
+import time
+from ConfigParser import ConfigParser
 from argparse import ArgumentParser, FileType
 from urlparse import urljoin
 
 
 class Nudibranch(object):
-    BASE_URL = 'http://localhost:6543'
-    #BASE_URL = 'https://borg.cs.ucsb.edu'
-    PATHS = {'auth':         'session',
-             'file_item':    'file/{sha1sum}/info',
-             'project_item': 'class/{class_name}/{project_id}',
-             'submission':   'submission'}
+    PATHS = {'auth':           'session',
+             'file_item':      'file/{sha1sum}',
+             'file_item_info': 'file/{sha1sum}/info',
+             'project_item':   'class/{class_name}/{project_id}',
+             'submission':     'submission'}
 
-    @classmethod
-    def url(cls, resource, **kwargs):
-        return urljoin(cls.BASE_URL, cls.PATHS[resource]).format(**kwargs)
+    @staticmethod
+    def _get_config(section):
+        config = ConfigParser()
+        if 'APPDATA' in os.environ:  # Windows
+            os_config_path = os.environ['APPDATA']
+        elif 'XDG_CONFIG_HOME' in os.environ:  # Modern Linux
+            os_config_path = os.environ['XDG_CONFIG_HOME']
+        elif 'HOME' in os.environ:  # Legacy Linux
+            os_config_path = os.path.join(os.environ['HOME'], '.config')
+        else:
+            os_config_path = None
+        locations = ['nudibranch.ini']
+        if os_config_path is not None:
+            locations.insert(0, os.path.join(os_config_path, 'nudibranch.ini'))
+        if not config.read(locations):
+            raise Exception('No nudibranch.ini found.')
+        if not config.has_section(section) and section != 'DEFAULT':
+            raise Exception('No section `{0}` found in nudibranch.ini.'
+                            .format(section))
+        return dict(config.items(section))
 
-    def __init__(self):
-        self.debug = True
+    def __init__(self, config_section):
+        config = self._get_config(config_section)
+        self.debug = config['debug'].lower() in ('1', 'true', 'yes')
+        self.request_delay = int(config['request_delay'])
+        self.request_timeout = int(config['request_timeout'])
+        self._url = config['url']
         self.session = requests.session()
 
     def login(self, username=None, password=None):
@@ -60,43 +83,32 @@ class Nudibranch(object):
                                 filenames=filenames)
         self.msg('Make submission: {0}'.format(response.status_code))
         if response.status_code == 201:
-            url = urljoin(self.BASE_URL, response.json['redir_location'])
+            url = urljoin(self._url, response.json['redir_location'])
             print('Submission successful')
             print('Results will be available at: {0}'.format(url))
             return 0
         return 3
 
     def request(self, url, method='get', **data):
-        method = method.lower()
-        if method == 'get':
-            assert len(data) == 0
-            # TERRIBLE HACK
-            # on my setup, without this sleep, there is almost always a
-            # 'Connection Reset by Peer' exception
-            import time
-            time.sleep(1)
-            response = self.session.get(url)
-        elif method == 'head':
-            assert len(data) == 0
-            response = self.session.head(url)
-        else:
-            response = getattr(self.session, method)(url, json.dumps(data),
-                                                     verify=False)
-        return response
+        time.sleep(self.request_delay)
+        args = (json.dumps(data),) if data else ()
+        return getattr(self.session, method.lower())(
+            url, *args, verify=False, timeout=self.request_timeout)
 
     def send_file(self, the_file):
         print('Sending {0}'.format(the_file.name))
         data = the_file.read()
         sha1sum = hashlib.sha1(data).hexdigest()
-        url = self.url('file_item', sha1sum=sha1sum)
+        test_url = self.url('file_item_info', sha1sum=sha1sum)
+        upload_url = self.url('file_item', sha1sum=sha1sum)
 
         # Have we already uploaded the file?
-        response = self.request(url, 'GET')
+        response = self.request(test_url, 'GET')
         self.msg('Test file: {0}'.format(response.status_code))
         if response.status_code == 200:
             return response.json['file_id']
         # Upload the file
-        response = self.request(url, 'PUT',
+        response = self.request(upload_url, 'PUT',
                                 b64data=base64.b64encode(data).decode('ascii'))
         self.msg('Send file: {0}'.format(response.status_code))
         if response.status_code == 200:
@@ -122,6 +134,9 @@ class Nudibranch(object):
             return 2
         return self.make_submission(project, file_mapping)
 
+    def url(self, resource, **kwargs):
+        return urljoin(self._url, self.PATHS[resource]).format(**kwargs)
+
     def verify_access(self, project):
         class_name, project_id = project.split(':')
         url = self.url('project_item', class_name=class_name,
@@ -133,11 +148,12 @@ class Nudibranch(object):
 
 def main():
     parser = ArgumentParser()
+    parser.add_argument('-c', '--config', default='DEFAULT')
     parser.add_argument('-p', '--project', required=True)
     parser.add_argument('files', nargs='+', type=FileType('rb'))
     args = parser.parse_args()
 
-    client = Nudibranch()
+    client = Nudibranch(args.config)
     return client.submit(args.project, args.files)
 
 
