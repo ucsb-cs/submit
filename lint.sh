3713232aa954fa8d2bf44de5ab0f41fe841c81ab
Bryce Boe <bbzbryce@gmail.com>
2013-01-16 18:07:54 -0800

    Use flake8 for lint testing.

--- lint.sh
+++ lint.sh
@@ -2,31 +2,20 @@
 
 dir=$(dirname $0)
 
-failure=0
-
-# pep8
-output=$(find $dir -name \*.py -exec pep8 {} \;)
-if [ -n "$output" ]; then
-    echo "---pep8----"
-    echo -e "$output"
-    failure=1
+# flake8 (runs pep8 and pyflakes)
+flake8 $dir/nudibranch
+if [ $? -ne 0 ]; then
+    echo "Exiting due to flake8 errors. Fix and re-run to finish tests."
+    exit $?
 fi
 
+# ignore pylint for now
+exit 0
+
 # pylint
-#output=$(pylint --rcfile=$dir/.pylintrc $dir/nudibranch 2> /dev/null)
-output=""
+output=$(pylint --rcfile=$dir/.pylintrc $dir/nudibranch 2> /dev/null)
 if [ -n "$output" ]; then
     echo "---pylint---"
     echo -e "$output"
-    failure=1
+    exit $?
 fi
-
-# pyflakes
-output=$(find $dir/nudibranch -name [A-Za-z_]\*.py -exec pyflakes {} \;)
-if [ -n "$output" ]; then
-    echo "---pyflakes---"
-    echo -e "$output"
-    failure=1
-fi
-
-exit $failure
