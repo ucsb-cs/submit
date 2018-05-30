50d55e58f6a09212f545baeebbe9070b560c26fd
Phill Conrad <pconrad@cs.ucsb.edu>
2018-05-30 15:31:34 -0700

    v 1.3.1 is a bug fix

--- submit/__init__.py
+++ submit/__init__.py
@@ -8,7 +8,7 @@ from .helpers import get_queue_func
 from .models import configure_sql, create_schema, populate_database
 from .security import get_user, group_finder
 
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 
 
 class Root(object):
